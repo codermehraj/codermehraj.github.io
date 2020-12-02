db18090f85b58ae178f8d106f99b3d4d8a7d7189
Md Mehrajul Islam <47929493+codermehraj@users.noreply.github.com>
2020-12-02 18:03:11 +0600

    Update Sorting student structure.c

--- Random Codes/Rafi Sir Assignment/Sorting student structure.c
+++ Random Codes/Rafi Sir Assignment/Sorting student structure.c
@@ -1,26 +1,40 @@
+/*
+    Name : Md. Mehrajul Islam
+    Reg. Number: 2019831074
+    Problem Number : 1
+    Problem : Write a code to sort the elements firstly according to num of problems solved. If number of
+              problems solved is equal then according to cgpa, if cgpa is equal then according to their registration number.
+*/
 #include <stdio.h>
 
 #define MX_SIZE 100000
 
-typedef struct student{
+typedef struct student
+{
     int NumberOfProblemSolved, RegNumber;
     double cgpa;
 };
 
 // returns 1 if A < B else 0
-int cmp(struct student A, struct student B){
-    if(A.NumberOfProblemSolved == B.NumberOfProblemSolved){
-        if(A.cgpa == B.cgpa){
+int cmp(struct student A, struct student B)
+{
+    if (A.NumberOfProblemSolved == B.NumberOfProblemSolved)
+    {
+        if (A.cgpa == B.cgpa)
+        {
             // registration number less is expected
-            if(A.RegNumber > B.RegNumber) return 1;
+            if (A.RegNumber > B.RegNumber)
+                return 1;
             return 0;
         }
         // cgpa more is expected
-        if(A.cgpa < B.cgpa) return 1;
+        if (A.cgpa < B.cgpa)
+            return 1;
         return 0;
     }
     // Number of Problem solved more is expected
-    if(A.NumberOfProblemSolved < B.NumberOfProblemSolved) return 1;
+    if (A.NumberOfProblemSolved < B.NumberOfProblemSolved)
+        return 1;
     return 0;
 }
 
@@ -33,7 +47,7 @@ void bubble_sort(struct student DATA[], int N)
         for (PTR = 0; PTR < (N - K); PTR++)
         {
             // comaprison
-            if (cmp(DATA[PTR] , DATA[PTR + 1])==1)
+            if (cmp(DATA[PTR], DATA[PTR + 1]) == 1)
             {
                 // interchange
                 temp = DATA[PTR];
@@ -51,15 +65,16 @@ int main()
     double cg;
 
     printf("\n\n\tEnter the number of students : ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
     struct student ar[n];
 
-    printf("\nEnter the information of %d students in following format: \n",n);
+    printf("\nEnter the information of %d students in following format: \n", n);
     printf("<Registration_Number>  <Number_of_solved_problems> <Cgpa>\n\n");
 
-    for(i=0;i<n;i++){
-        scanf("%d%d%lf",&reg,&prblmslved,&cg);
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d%d%lf", &reg, &prblmslved, &cg);
         ar[i].RegNumber = reg;
         ar[i].NumberOfProblemSolved = prblmslved;
         ar[i].cgpa = cg;
@@ -69,8 +84,9 @@ int main()
     bubble_sort(ar, n);
 
     printf("\n After Sorting the list is as follows : \n\n");
-    for(i=0;i<n;i++){
-        printf("\t%d\t%d\t%lf\n",ar[i].RegNumber,ar[i].NumberOfProblemSolved,ar[i].cgpa);
+    for (i = 0; i < n; i++)
+    {
+        printf("\t%d\t%d\t%lf\n", ar[i].RegNumber, ar[i].NumberOfProblemSolved, ar[i].cgpa);
     }
     return 0;
 }
